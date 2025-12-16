43d5448117057ce673f55fc85a9be74d0d282c01
Adib <adibwatfa341@gmail.com>
2025-12-16 09:00:19 +0300

    Configured docker compose and checks to wait for db2

--- app/app/tests.py
+++ app/app/tests.py
@@ -9,16 +9,14 @@ from app import calc
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
 
-
     def test_add_numbers(self):
         """Test adding the numbers together."""
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
-
-    def test_subtrac_number(self):
-        """Test subtracting number."""
+    def test_subtract_numbers(self):
+        """Test subtracting numbers."""
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
